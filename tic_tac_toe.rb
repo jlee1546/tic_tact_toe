57e27b0a67a0dda47a6c4905e7ebe354bb9a65b3
Jonathan Lee <jlee1546.jl@gmail.com>
2022-09-03 13:19:16 -0400

    Add play_round method, ability to clear console, game asthetics

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,5 @@
 
-
+# GameBoard class contains game board and win logic
 class GameBoard
   
   @squares = {1=>'1', 2=>'2', 3=>'3', 4=>'4', 5=>'5', 6=>'6',
@@ -19,7 +19,12 @@ class GameBoard
   end
 
   def self.place_marker(marker, number)
-    @squares[number] = marker
+    if @squares[number] == 'X' || @squares[number] == 'O'
+      puts "That is already occupied. Select another space."
+    else
+      @squares[number] = marker
+    end
+    
   end
 
   def self.check_win
@@ -27,20 +32,29 @@ class GameBoard
     @squares.each do |key,value|
       array.push(value)
     end
-    if [array[0],array[1],array[2]].all?('X') || [array[0],array[1],array[2]].all?('O') ||
-      [array[3],array[4],array[5]].all?('X') || [array[3],array[4],array[5]].all?('O') ||
-      [array[6],array[7],array[8]].all?('X') || [array[6],array[7],array[8]].all?('O') ||
-      [array[0],array[3],array[6]].all?('X') || [array[0],array[3],array[6]].all?('O') ||
-      [array[1],array[4],array[7]].all?('X') || [array[1],array[4],array[7]].all?('O') ||
-      [array[2],array[5],array[8]].all?('X') || [array[2],array[5],array[8]].all?('O') ||
-      [array[0],array[4],array[8]].all?('X') || [array[0],array[4],array[8]].all?('O') ||
-      [array[2],array[4],array[6]].all?('X') || [array[2],array[4],array[6]].all?('O')
-      
+    if [array[0],array[1],array[2]].all?('X') || [array[3],array[4],array[5]].all?('X') || 
+      [array[6],array[7],array[8]].all?('X') || [array[0],array[3],array[6]].all?('X') || 
+      [array[1],array[4],array[7]].all?('X') || [array[2],array[5],array[8]].all?('X') ||
+      [array[0],array[4],array[8]].all?('X') || [array[2],array[4],array[6]].all?('X') 
+
+      puts "We have a winner!"
+      return true
+
+    elsif [array[0],array[1],array[2]].all?('O') || [array[3],array[4],array[5]].all?('O') || 
+      [array[6],array[7],array[8]].all?('O') || [array[0],array[3],array[6]].all?('O') || 
+      [array[1],array[4],array[7]].all?('O') || [array[2],array[5],array[8]].all?('O') || 
+      [array[0],array[4],array[8]].all?('O') || [array[2],array[4],array[6]].all?('O') 
+      puts "We have a winner!"
+      return true
+
+    elsif (array.uniq.length == 2)
+      puts "The Cat has this game!"
       return true
     end
   end
 end
 
+# Player class to instantiate a player
 class Player
 
   def initialize(marker)
@@ -63,10 +77,31 @@ class Player
 
 end
 
+# function to play a round of tic tac toe 
+def play_round(player_string, player_number)
+  
+  puts "#{player_string} please choose a square"
+  while number = gets.chomp.to_i
+    break if GameBoard.place_marker(player_number.place_marker, number)
+  end
+  system ('clear')
+  GameBoard.print_game_board
+  puts " "
+  puts " "
+  if GameBoard.check_win
+    return
+  end
+end
+
+# GAMEPLAY BEGINS BELOW
+
+# initial set up assigning X or O to player 1 and 2
+puts " "
+puts " "
 puts 'Will Player 1 be X or O?'
 player_one_marker = gets.chomp
 
-player_two_marker = player_one_marker == 'X' ? 'O' : 'X'
+player_one_marker == 'X' ? player_two_marker = 'O' : player_two_marker = 'X'
 puts "Okay! Player 2 will be #{player_two_marker}"
 
 player_one = Player.new(player_one_marker)
@@ -75,22 +110,12 @@ player_two = Player.new(player_two_marker)
 won = false
 
 GameBoard.print_game_board
+puts " "
+puts " "
 
+# Game loop calling 2 instances of play_oround with info for player 1 and then player 2
 while !won do
-  puts 'Player 1 go!'
-  number = gets.chomp.to_i
-
-  GameBoard.place_marker(player_one.place_marker, number)
-  GameBoard.print_game_board
-  break if GameBoard.check_win
-
-  puts 'Player 2 go!'
-  number = gets.chomp.to_i
-  GameBoard.place_marker(player_two.place_marker,number)
-  GameBoard.print_game_board
-  break if GameBoard.check_win
+  play_round('Player 1',player_one)
+  play_round('Player 2',player_two)
   
 end
-
-
-
